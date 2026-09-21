bfca4fe52f07be62095f2391ba9723cbd924c4b6
thelast12 <71203697+thelast12@users.noreply.github.com>
2026-09-21 12:19:53 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'taxcalc-usa-v3';
+const APP_SHELL = [
+  '/', '/overtime/', '/self-employed/', '/tax-refund/', '/state-compare/',
+  '/converter/', '/bonus-tax/', '/w4-helper/', '/401k-ira/', '/manifest.json', '/favicon.svg'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match('/'))));
+});
